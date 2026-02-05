83992b6d0ad5fbc481e428cc876ceab6835a1c3d
Jay <jiajun181@gmail.com>
2026-02-05 12:09:02 -0500

    hardcode uploadthing token to bypass env var issue

--- app/api/uploadthing/route.js
+++ app/api/uploadthing/route.js
@@ -1,9 +1,12 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
-// 通行证
 export const runtime = 'edge';
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    // 👇 我们直接把密码写在这里，跳过环境变量检查！
+    token: "eyJhcGlLZXkiOiJza19saXZlXzI3ZDRjMmM3ZDFkZTA1MDE2ZmYyNjBjNmQxOGZlNTBjNWU4ZmMxYzA1NDE1MTFlYjZhMmYwMmYwZGJiZThmZTgiLCJhcHBJZCI6InVqcHNhdWV0ODkiLCJyZWdpb25zIjpbInNlYTEiXX0=",
+  },
 });
